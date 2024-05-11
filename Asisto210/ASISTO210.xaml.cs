653eeb76db83ffae8718af334c1b3b0a72652608
Mauricio Leonardo Alemán Díaz <143914334+MauricioAlemanD@users.noreply.github.com>
2024-05-11 16:03:11 -0600

    Agregar archivos de proyecto.

---
+++ Asisto210/ASISTO210.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Asisto210
+{
+    /// <summary>
+    /// Lógica de interacción para ASISTO210.xaml
+    /// </summary>
+    public partial class ASISTO210 : Window
+    {
+        public ASISTO210()
+        {
+            InitializeComponent();
+        }
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void ListViewItemuseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Asisto210/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Asisto210
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void txtUsuario_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtUsuario.Text = "";
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            lblContraseña.Visibility = Visibility.Hidden;
+            txtContraseña.Focus();
+        }
+
+        private void btnIngresar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            bBtnIngresar.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFD45C5C"));
+        }
+
+        private void btnIngresar_MouseLeave(object sender, MouseEventArgs e)
+        {
+            bBtnIngresar.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF4E1417"));
+        }
+
+        private void Label_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Label_MouseDown_2(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void txtContraseña_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            lblContraseña.Visibility= Visibility.Hidden;
+        }
+
+        private void btnIngresar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ASISTO210 aSISTO210 = new ASISTO210();
+            aSISTO210.Show();
+            this.Hide();    
+        }
+
+        private void txtUsuario_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
