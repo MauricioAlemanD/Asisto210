3e6dce7abc1deff40d24f558e2ba1fa67dc792fd
Mauricio Leonardo Alemán Díaz <143914334+MauricioAlemanD@users.noreply.github.com>
2024-06-03 00:28:21 -0600

    personal 90

--- Asisto210/Personal.xaml.cs
+++ Asisto210/Personal.xaml.cs
@@ -16,6 +16,9 @@ using System.Collections;
 using System.Data.SqlTypes;
 using System.Data.SqlClient;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using System.Data;
+using System.Reflection;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 
@@ -37,11 +40,7 @@ namespace Asisto210
             EncabezadosTablaBusquedaPersonal();
             
             //llenadoBusquedaPersonal();
-
-
         }
-
-
         private void EncabezadosTablaBusquedaPersonal()
         {
             DataGridTextColumn idColumn = new DataGridTextColumn
@@ -80,23 +79,24 @@ namespace Asisto210
             };
             dvgBusquedaPersonal.Columns.Add(rolColumn);
         }
-
         private void txtNombre_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
-
         private void Page_Initialized(object sender, EventArgs e)
+        {
+            actrualizarPersonal();
+        }
+        private void actrualizarPersonal()
         {
             llenadoTablaPersonal();
             llenadoCMBRoles();
+            llenadoCMBPErsonal();
         }
-
         private void txtBusquedaPersonal_GotFocus(object sender, RoutedEventArgs e)
         {
             txtBusquedaPersonal.Text = string.Empty;
         }
-
         private void btnAñadir_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             string cvePersonal = "";
@@ -123,10 +123,9 @@ namespace Asisto210
 
             añadirPersonal(cvePersonal, nombre, apellidoPaterno, apellidoMaterno, rol);
         }
-
         private void llenadoTablaPersonal()
         {
-            string query = "select personal.cve_personal,personal.nombre,personal.apelldio_pateno,personal.apellido_materno,roles.descripcion from personal\r\ninner join roles on personal.rol_personal = roles.cve_rol";
+            string query = "select personal.cve_personal,personal.nombre,personal.apelldio_pateno,personal.apellido_materno,roles.descripcion from personal inner join roles on personal.rol_personal = roles.cve_rol";
             List<TablaBusquedaPersonal> lTablaBusquedaPersonal = new List<TablaBusquedaPersonal>();
 
             using (var reader = conexion.ExecuteReader(query))
@@ -145,9 +144,9 @@ namespace Asisto210
                 dvgBusquedaPersonal.ItemsSource = lTablaBusquedaPersonal;
             }
         }
-
         private void llenadoCMBRoles()
         {
+            cmbRoles.Items.Clear();
             string query = "Select descripcion from roles";
 
             using (var reader = conexion.ExecuteReader(query))
@@ -155,13 +154,29 @@ namespace Asisto210
                 while (reader.Read())
                 {
                     cmbRoles.Items.Add(reader["descripcion"].ToString());
+                    cmbRoles_Editar.Items.Add(reader["descripcion"].ToString());
                 }
             }
         }
+        private void llenadoCMBPErsonal()
+        {
+            cmbPersonal_Eliminar.Items.Clear();
+            string query = "select personal.cve_personal,personal.nombre,personal.apelldio_pateno,personal.apellido_materno,roles.descripcion from personal inner join roles on personal.rol_personal = roles.cve_rol";
 
+            using (var reader = conexion.ExecuteReader(query))
+            {
+                string personal = "";
+                while (reader.Read())
+                {
+                    personal = "";
+                    personal = reader["cve_personal"].ToString() + " " + reader["nombre"].ToString() + " " + reader["apelldio_pateno"].ToString() + " " + reader["apellido_materno"].ToString() + " " + reader["descripcion"].ToString();
+                    cmbPersonal_Eliminar.Items.Add(personal);
+                    cmbPersonal_Editar.Items.Add(personal);
+                }
+            }
+        }
         private void añadirPersonal(string cvePersonal, string nombre, string apellidoPaterno, string apellidoMaterno, string rol)
         {
-
             string query = "INSERT INTO personal (cve_personal, nombre, apelldio_pateno, apellido_materno, rol_personal) " +
                    "VALUES (@cve_personal, @nombre, @apelldio_pateno, @apellido_materno, @rol_personal)";
 
@@ -185,7 +200,6 @@ namespace Asisto210
                 MessageBox.Show("Error al añadir personal: " + ex.Message);
             }
         }
-
         private string obtenerUltimaID()
         {
             string query = "SELECT TOP 1 [cve_personal] FROM [dbo].[personal] ORDER BY [cve_personal] DESC";
@@ -204,6 +218,69 @@ namespace Asisto210
 
             return nuevaId;
         }
+        private void btnEliminarPersonal_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            string cve_eliminar = "0000";
+            string cmbEliminar = "";
+            cmbEliminar =  cmbPersonal_Eliminar.SelectedItem.ToString();
+            cve_eliminar = cmbEliminar.Substring(0, 4);
+
+            string query = "DELETE FROM personal WHERE cve_personal = @cve_personal";
+
+            SqlParameter[] parameters = {
+                new SqlParameter("@cve_personal", cve_eliminar)
+            };
+
+            try
+            {
+                // Pasar el array de parámetros a ExecuteNonQuery
+                conexion.ExecuteNonQuery(query, parameters);
+                
+            }
+            catch (Exception ex)
+            {
+                // Manejo de excepción, mostrar el error o registrar
+                MessageBox.Show("Error al eliminar personal: " + ex.Message);
+            }
+
+            actrualizarPersonal();
+        }
+
+        private void cmbPersonal_Editar_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            string cve_editar = "0000";
+            string cmbEditar = "";
+            cmbEditar = cmbPersonal_Editar.SelectedItem.ToString();
+            cve_editar = cmbEditar.Substring(0, 4);
+
+            string query = "select personal.nombre,personal.apelldio_pateno,personal.apellido_materno,roles.descripcion from personal inner join roles on personal.rol_personal = roles.cve_rol where personal.cve_personal = '"+cve_editar+"'";
+
+            using (var reader = conexion.ExecuteReader(query))
+            {
+                while (reader.Read())
+                {
+                    txtNombre_Editar.Text = reader["nombre"].ToString();
+                    txtApellidoPaterno_Editar.Text = reader["apelldio_pateno"].ToString();
+                    txtApellidoMaterno_Editar.Text = reader["apellido_materno"].ToString();
+                    if (reader["descripcion"].ToString() == "Directivo")
+                    {
+                        cmbRoles_Editar.SelectedItem = 1;
+                    }
+                    else if ((reader["descripcion"].ToString()) == "Administrador")
+                    {
+                        cmbRoles_Editar.SelectedItem = 2;
+                    }
+                    else if ((reader["descripcion"].ToString()) == "Docente")
+                    {
+                        cmbRoles_Editar.SelectedItem = 3;
+                    }
+                    cmbRoles_Editar.Text = reader["descripcion"].ToString();
+                }
+            }
+        }
+
+
 
     } // End class
     }// End namespace
